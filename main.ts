57bcbb1ce9ee3282fc781e9d5bf43365e211788e
Attituding <84680760+Attituding@users.noreply.github.com>
2021-10-19 19:15:40 -0700

    Bot logs in

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
-const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
+import { Client, Intents } from 'discord.js';
+import { discordAPIkey as token } from './config.json';
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log(`Logged in as ${client.user.tag}!`);
+	client.user.setStatus('dnd');
 });
 
 client.login(token);
