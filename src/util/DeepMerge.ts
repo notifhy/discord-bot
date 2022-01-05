39956e070b30cb704c79009df98b32f0851f79ac
Attituding <84680760+Attituding@users.noreply.github.com>
2022-01-05 12:48:19 -0800

    feat: deep merge algorithm

---
+++ src/util/DeepMerge.ts
@@ -0,0 +1,59 @@
+function isObject(value: unknown): value is Record<string, unknown> {
+    const type = Object.prototype.toString.call(value as string);
+    return type === '[object Object]';
+}
+
+export function merge<
+    Target extends Record<string, unknown>,
+    Value extends Record<string, unknown>
+>(target: Target, object: Value) {
+    const keys = Object.keys(target).concat(Object.keys(object));
+
+    for (const key of keys) {
+        if (
+            Array.isArray(target[key]) &&
+            Array.isArray(object[key])
+        ) {
+            const targetKeys = Object.keys(target[key] as unknown[]);
+            const objectKeys = Object.keys(object[key] as unknown[]);
+
+            for (
+                let i = 0;
+                i < Math.max(targetKeys.length, objectKeys.length);
+                i += 1
+            ) {
+                if (
+                    isObject((target[key] as unknown[])?.[i]) &&
+                    isObject((object[key] as unknown[])?.[i])
+                ) {
+                    Object.assign(
+                        (target[key] as unknown[])[i],
+                        (object[key] as unknown[])[i],
+                    );
+                } else {
+                    Array.from(
+                        new Set((target[key] as unknown[]).concat(object[key])),
+                    );
+                }
+            }
+        } else if (
+            isObject(target[key]) &&
+            isObject(object[key])
+        ) {
+            merge(
+                target[key] as Record<string, unknown>,
+                object[key] as Record<string, unknown>,
+            );
+        } else if (
+            object[key] !== undefined &&
+            target[key] === undefined
+        ) {
+            Object.assign(
+                target,
+                { [key]: object[key] },
+            );
+        }
+    }
+
+    return target;
+}
