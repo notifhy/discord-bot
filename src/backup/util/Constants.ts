b2b83a8ea84da7fd37592961d191b665068ca296
Attituding <84680760+Attituding@users.noreply.github.com>
2022-02-03 17:31:08 -0800

    test: Backup interval to 30 minutes

--- src/backup/util/Constants.ts
+++ src/backup/util/Constants.ts
@@ -1,5 +1,5 @@
 export default {
-    interval: 1000 * 60 * 15,
+    interval: 1000 * 60 * 30,
     parentFolder: '15KvOYG6r6To_fgXf5DOkIT6aEcUtT5rg',
     scopes: ['https://www.googleapis.com/auth/drive'],
 };
