35da588fd2c3c34ebb4bf6e8a199f865dd84794b
Attituding <84680760+Attituding@users.noreply.github.com>
2022-02-11 12:18:56 -0800

    test: reduce backups to every hour

--- src/Backup/util/Constants.ts
+++ src/Backup/util/Constants.ts
@@ -1,5 +1,5 @@
 export const Constants = {
-    interval: 1000 * 60 * 30,
+    interval: 1000 * 60 * 60,
     parentFolder: '15KvOYG6r6To_fgXf5DOkIT6aEcUtT5rg',
     scopes: ['https://www.googleapis.com/auth/drive'],
 };
