07c48bc1d803f564dcd5e60e0d32d0e5cacc6fbc
Attituding <84680760+Attituding@users.noreply.github.com>
2022-10-17 20:49:41 -0700

    fix: add missing modules

---
+++ src/modules/Friends.ts
@@ -0,0 +1,20 @@
+import { type users as User } from '@prisma/client';
+import { type CleanHypixelData } from '../@types/Hypixel';
+import { type Changes } from '../core/Data';
+import { Module } from '../structures/Module';
+
+export class FriendsModule extends Module {
+    public constructor(context: Module.Context, options: Module.Options) {
+        super(context, {
+            ...options,
+            name: 'friends',
+            databaseColumn: 'friendsModule',
+            localization: 'modulesFriendsName',
+            requireStatusAPI: true,
+        });
+    }
+
+    public async run(user: User, newData: CleanHypixelData, changes: Changes) {
+
+    }
+}
---
+++ src/modules/Rewards.ts
@@ -0,0 +1,20 @@
+import { type users as User } from '@prisma/client';
+import { type CleanHypixelData } from '../@types/Hypixel';
+import { type Changes } from '../core/Data';
+import { Module } from '../structures/Module';
+
+export class RewardsModule extends Module {
+    public constructor(context: Module.Context, options: Module.Options) {
+        super(context, {
+            ...options,
+            name: 'rewards',
+            databaseColumn: 'rewardsModule',
+            localization: 'modulesRewardsName',
+            requireStatusAPI: true,
+        });
+    }
+
+    public async run(user: User, newData: CleanHypixelData, changes: Changes) {
+
+    }
+}
